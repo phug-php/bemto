400e0e0f13483a3cdc67d56055ada4848f842c01
KyleKatarn <kylekatarnls@gmail.com>
2018-11-12 18:09:31 +0100

    Add JS/PHP tests

--- tests/RenderTest.php
+++ tests/RenderTest.php
@@ -2,11 +2,33 @@
 
 namespace Tests\Bemto;
 
+use JsPhpize\JsPhpizePhug;
+use Phug\Renderer;
+use PhugBemto\PhugBemto;
+
 class RenderTest extends AbstractTestCase
 {
+    public function getJsCases()
+    {
+        $this->renderer = new Renderer([
+            'debug' => true,
+            'execution_max_time' => 180000,
+            'modules' => [
+                JsPhpizePhug::class,
+                PhugBemto::class,
+            ],
+        ]);
+        foreach (glob(__DIR__ . '/cases/*.html') as $file) {
+            yield [
+                file_get_contents($file),
+                preg_replace('/\.html$/', '.pug', $file),
+            ];
+        }
+    }
+
     public function getCases()
     {
-        foreach (glob(__DIR__ . '/cases/*.html') as $file) {
+        foreach (glob(__DIR__ . '/cases-php/*.html') as $file) {
             yield [
                 file_get_contents($file),
                 preg_replace('/\.html$/', '.pug', $file),
@@ -18,6 +40,14 @@ class RenderTest extends AbstractTestCase
      * @dataProvider getCases
      */
     public function testRender($expectedOutput, $sourceFile)
+    {
+        $this->assertSameHtml($expectedOutput, $this->renderFile($sourceFile));
+    }
+
+    /**
+     * @dataProvider getJsCases
+     */
+    public function testRenderJs($expectedOutput, $sourceFile)
     {
         $this->assertSameHtml($expectedOutput, $this->renderFile($sourceFile));
     }
